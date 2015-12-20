2ddfeff3e140c26c9243b51da2d67233ca3bcd69
kwok.ipromise@gmail.com <kwok.ipromise@gmail.com>
2015-12-20 01:08:22 -0500

    fixed issue with SQLSTATE error & added mongodb php driver

--- misc/make.sh
+++ misc/make.sh
@@ -11,15 +11,21 @@ export OPENSHIFT_RUNTIME_DIR=${OPENSHIFT_HOMEDIR}/app-root/runtime
 export ROOT_DIR=${OPENSHIFT_RUNTIME_DIR}	#CARTRIDGE
 export LIB_DIR=${ROOT_DIR}/lib
 export CONF_DIR=${OPENSHIFT_REPO_DIR}/conf
-
+export MONGODB_DIR=${OPENSHIFT_RUNTIME_DIR}/srv/mongodb
 
 export DIST_PHP_VER=5.5.18
 
 cd $OPENSHIFT_RUNTIME_DIR
 mkdir srv
 mkdir srv/libmcrypt
+mkdir srv/mongodb
+
+mkdir srv/libsasl2
+#mkdir srv/libsasl2-devel-2.1.26-4
 mkdir tmp
 cd tmp
+
+
 				
 wget http://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz
 tar -zxf libmcrypt-2.5.8.tar.gz
@@ -29,13 +35,44 @@ cd libmcrypt-2.5.8
 --disable-posix-threads
 make && make install
 cd ..
-				
-rm -f -r libmcrypt-2.5.8
-				
+rm -f -r libmcrypt-2.5.8				
 rm -r $OPENSHIFT_RUNTIME_DIR/tmp/*.tar.gz
 
 
+wget ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz
+tar -zxf cyrus-sasl-2.1.26.tar.gz
+cd cyrus-sasl-2.1.26
+./configure \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/libsasl2
+make && make install
+export MONGODB_SASL=${OPENSHIFT_RUNTIME_DIR}/srv/libsasl2
+export SASL_PATH=${OPENSHIFT_RUNTIME_DIR}/srv/libsasl2
+cd ..
 
+#wget ftp://ftp.dvo.ru/pub/cygwin/64bit/release/cyrus-sasl/libsasl2-devel/libsasl2-devel-2.1.26-4.tar.bz2
+#tar -jxf libsasl2-devel-2.1.26-4.tar.bz2
+#cd libsasl2-devel-2.1.26-4
+
+#cd ..
+
+
+
+git clone https://github.com/mongodb/mongo-php-driver.git phongo
+cd phongo
+git submodule update --init
+mkdir ./include/sasl
+cp $SASL_PATH/include/sasl/*.* ./include/sasl
+cp -f ../../repo/misc/file/sasl.h ./include/sasl/sasl.h
+./configure \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/mongodb \
+--enable-developer-flags \
+--with-mongodb-sasl=$SASL_PATH
+make -j8 all
+cp ./modules/*.* ../../srv/mongodb
+
+cd..
+
+rm -f -r phongo
 
 
 pushd ${OPENSHIFT_REPO_DIR}/misc
@@ -66,7 +103,7 @@ mkdir bootstrap/cache
 chmod -R 2775 bootstrap/cache
 chmod -R 2775 storage
 
-chmod 2775 .env-dist
+chmod -R 2777 .env-dist
 sed -i '/DB_DRIVER=/ c\DB_DRIVER=mysql' .env-dist
 sed -i '/DB_HOST=/ c\DB_HOST='$OPENSHIFT_MYSQL_DB_HOST'' .env-dist
 sed -i '/DB_PORT=/ c\DB_PORT='$OPENSHIFT_MYSQL_DB_PORT'' .env-dist
@@ -75,9 +112,20 @@ sed -i '/DB_PASSWORD=/ c\DB_PASSWORD='$OPENSHIFT_MYSQL_DB_PASSWORD'' .env-dist
 sed -i '/DB_DATABASE=/ c\DB_DATABASE='$OPENSHIFT_APP_NAME'' .env-dist
 
 cp .env-dist .env
+chmod -R 0600 .env-dist
+chmod -R 0600 .env
 
 cd ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo
 
 rm -f -r www
 
 ln -s ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/dreamfactory/public www
+
+cd dreamfactory
+chmod -R 2755 composer.json
+sed -i '/dist/ c\  "preferred-install": "source"' composer.json
+chmod -R 0700 composer.json
+
+php $OPENSHIFT_DATA_DIR/bin/composer install --no-dev
+
+
--- www/index.cgi
+++ www/index.cgi
@@ -11,7 +11,7 @@ fi
 
 if [[ "$QUERY_STRING" =~ "doitnow" ]]; then
 	chmod +x ${OPENSHIFT_REPO_DIR}/misc/make.sh
-	nohup ${OPENSHIFT_REPO_DIR}/misc/make.sh > /tmp/makephp &
+	nohup ${OPENSHIFT_REPO_DIR}/misc/make.sh > /misc/make_log &
 	sleep 1
 	echo "Location: ./?working
 
@@ -29,20 +29,18 @@ X-Powered-By: /bin/bash
 <title>Installed</title>
 </head>
 <body>
-<h1>Installed</h1>
-<p>Building PHP5.5 + Apache + Dreamfactory</p>
+<h1>Installation</h1>
+<p>Installing PHP5.5 + Apache + Dreamfactory</p>
 <h2>Next...</h2>
 <p>"
 
 if [[ -x ${OPENSHIFT_RUNTIME_DIR}/bin/php-cgi ]]; then
 	echo "Start coding or test <a href=\"?phpinfo\">phpinfo</a>. <b>Remember to remove index.cgi</b>"
-elif [[ -f /tmp/makephp ]]; then
+elif [[ -f /misc/make_log ]]; then
 	echo "<p>Still spawning your world...</p>"
 	echo "<p>This page shall refresh automatically...</p>"
 	echo "<p>Now you can close this page, and come back whenever...</p>"
-	echo "<p>Have problem? <a href=https://github.com/laobubu/openshift-php5.5-cgi-apache/issues/new >submit issue.</a></p>"
-	echo "<pre style='font-size:.7em;word-break:break-all;font-family:Courier'>"
-	tail -1000 /tmp/makephp
+	tail -1000 /misc/make_log
 	echo "</pre>"
 	echo "<script>setTimeout(function(){window.location.reload(true)},10000)</script>"
 else
